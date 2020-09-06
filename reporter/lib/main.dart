b6dbe72f805fddc0d264b95e25a4a16a1c265d06
Arie Field <arie.field@gmail.com>
2020-09-05 21:55:29 -0400

    Groundwork

--- reporter/lib/main.dart
+++ reporter/lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:developer';
+import 'dart:async';
+
+import 'common/button.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,24 +14,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'OD Report',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          // primarySwatch: Colors.deepPurple,
+          primaryColor: Color(0xff6200EE)),
+      home: MyHomePage(title: 'Overdose Incident Form'),
     );
   }
 }
@@ -51,17 +52,12 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  Completer<GoogleMapController> _controller = Completer();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -77,41 +73,62 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: Column(children: [
+        Container(
+          padding: const EdgeInsets.only(top: 16, right: 16, left: 16),
+          child: Text(
+            'Fill out the form manually or scan the ambulance call report (ACR) if you have it on hand.',
+            style: TextStyle(fontSize: 16.0),
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Text(
+            'Scanning an ACR will allow the app to extract data from the report to automatically fill out the form. ',
+            style: TextStyle(fontSize: 16.0),
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: Button(
+            callback: () => log('Scan button pressed'),
+            title: "Scan ACR Report",
+          ),
+        ),
+        Divider(
+          color: Colors.grey,
+          height: 40,
+          thickness: 0.3,
+          indent: 10,
+          endIndent: 10,
+        ),
+        TextField(
+          decoration: InputDecoration(
+              border: InputBorder.none, hintText: 'Date & time of overdose'),
+        ),
+        TextField(
+          decoration: InputDecoration(
+              border: InputBorder.none, hintText: 'Choose a location'),
+        ),
+        Container(
+          height: 150,
+          child: GoogleMap(
+            mapType: MapType.hybrid,
+            initialCameraPosition: _kGooglePlex,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            },
+          ),
+        ),
+        TextField(
+          decoration:
+              InputDecoration(border: InputBorder.none, hintText: 'Comments'),
+        ),
+        Button(
+          callback: () => log('Submit pressed'),
+          title: "Submit Form",
+        ),
+      ]), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
